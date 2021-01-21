f99f279988f4f72f00f3570ff7dcf64ffb990a91
Karol.Qu <13821056120@139.com>
2021-01-21 16:31:35 +0800

    添加bloom测试实例

---
+++ bloom/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/tal-tech/go-zero/core/bloom"
+	"github.com/tal-tech/go-zero/core/stores/redis"
+)
+
+func main() {
+	store := redis.NewRedis("localhost:6379", "node")
+	filter := bloom.New(store, "testbloom", 1024)
+	filter.Add([]byte("kevin"))
+	filter.Add([]byte("wan"))
+
+	fmt.Println(filter.Exists([]byte("kevin")))
+	fmt.Println(filter.Exists([]byte("wan")))
+	fmt.Println(filter.Exists([]byte("nothing")))
+	fmt.Println(filter.Exists([]byte("kevin")))
+	fmt.Println(filter.Exists([]byte("wanfs")))
+	fmt.Println(filter.Exists([]byte("nothings")))
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/golang/protobuf v1.4.2
 	github.com/tal-tech/go-zero v1.1.4
+	go.etcd.io/etcd v0.0.0-20200402134248-51bdeb39e698
 	google.golang.org/grpc v1.29.1
 	google.golang.org/protobuf v1.25.0
 )
